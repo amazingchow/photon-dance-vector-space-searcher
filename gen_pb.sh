edd69c249d0833ef4bfe385ccd736cfc6cc0d6f9
zhoujian2 <zhoujian2@sensetime.com>
2020-06-23 10:47:50 +0800

    update pb for grpc gateway

--- gen_pb.sh
+++ gen_pb.sh
@@ -2,13 +2,17 @@
 # libprotoc 3.12.0
 # protoc-gen-go v1.24.0
 
-basepath=$(cd `dirname $0`; pwd)
-
-cd $basepath
-for i in $(ls $basepath/pb/*.proto); do
-	echo $i
-	fn=$(basename "$i")
-	/usr/local/bin/protoc -I/usr/local/include -I. \
-        --proto_path=$basepath/pb \
-        --go_out=$basepath/ "$fn"
+cd $GOMODULEPATH
+for i in $(ls $GOMODULEPATH/github.com/amazingchow/engine-vector-space-search-service/pb/*.proto); do
+	fn=github.com/amazingchow/engine-vector-space-search-service/pb/$(basename "$i")
+	echo "compile" $fn
+	/usr/local/bin/protoc -I/usr/local/include -I . \
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--go_out=plugins=grpc:. "$fn"
+	/usr/local/bin/protoc -I/usr/local/include -I . \
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--grpc-gateway_out=logtostderr=true:. "$fn"
+	/usr/local/bin/protoc -I/usr/local/include -I . \
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--swagger_out=logtostderr=true:. "$fn"
 done
